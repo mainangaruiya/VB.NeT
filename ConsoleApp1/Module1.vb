eeba02db5494f1a5047b1c437a261ae7c18e96e0
Maina Ngaruiya <100405059+mainangaruiya@users.noreply.github.com>
2024-05-28 21:28:57 +0300

    vb.net

---
+++ ConsoleApp1/Module1.vb
@@ -0,0 +1,12 @@
+﻿Option Explicit Off
+Module Program
+    Sub Main()
+        Dim X As Double = 3.142376
+        Dim y As Integer
+
+        y = X
+
+        Console.Write("y=" & y)
+        Console.ReadKey()
+    End Sub
+End Module
---
+++ ConsoleApp2/Module1.vb
@@ -0,0 +1,13 @@
+﻿Module Module1
+
+    Sub Main()
+        For y = 1 To 5 Step 1
+            For x = 1 To 5 Step 1
+                Console.WriteLine(" " & x)
+
+            Next
+            Console.WriteLine()
+        Next
+    End Sub
+
+End Module
